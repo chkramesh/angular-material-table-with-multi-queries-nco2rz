0d53a7846c84f8eb8b7d230d04e9c573f6cf5df9
chkramesh <chkramesh@gmail.com>
2019-05-05 08:54:28 -0400

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -55,16 +55,16 @@ const routes: Routes = [
 ];
 
 // Stackbliz
-import {A11yModule} from '@angular/cdk/a11y';
-import {BidiModule} from '@angular/cdk/bidi';
-import {ObserversModule} from '@angular/cdk/observers';
-import {OverlayModule} from '@angular/cdk/overlay';
-import {PlatformModule} from '@angular/cdk/platform';
-import {PortalModule} from '@angular/cdk/portal';
-import {ScrollDispatchModule} from '@angular/cdk/scrolling';
-import {CdkStepperModule} from '@angular/cdk/stepper';
-import {CdkTableModule} from '@angular/cdk/table';
-import {CdkTreeModule} from '@angular/cdk/tree';
+// import {A11yModule} from '@angular/cdk/a11y';
+// import {BidiModule} from '@angular/cdk/bidi';
+// import {ObserversModule} from '@angular/cdk/observers';
+// import {OverlayModule} from '@angular/cdk/overlay';
+// import {PlatformModule} from '@angular/cdk/platform';
+// import {PortalModule} from '@angular/cdk/portal';
+// import {ScrollDispatchModule} from '@angular/cdk/scrolling';
+// import {CdkStepperModule} from '@angular/cdk/stepper';
+// import {CdkTableModule} from '@angular/cdk/table';
+// import {CdkTreeModule} from '@angular/cdk/tree';
 
 // import {
 //   MatAutocompleteModule,
